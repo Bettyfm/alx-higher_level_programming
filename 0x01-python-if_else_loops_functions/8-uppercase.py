74def0bade93eef8d8cc53a5e6f57b70beb40d75
Bettyfm <bettyfraigui@gmail.com>
2023-11-28 18:43:12 -0800

    task9

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+def islower(c):
+    if ord(c) in range(97, 123):
+        return True
+    else:
+        return False
+
+
 def uppercase(str):
+    for c in str:
+        C = c
+        if islower(c):
+            C = chr(ord(c) - 32)
+            print("{}".format(C), end="")
+    print("")
