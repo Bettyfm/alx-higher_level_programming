0586a9f6bbbe23358f09f66f7dbe77598b49a7da
Bettyfm <bettyfraigui@gmail.com>
2023-11-30 15:13:40 -0800

    test

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     argv = sys.argv
     length = len(argv)
     count = 0
-    for i in range (1, length):
+    for i in range(1, length):
         count += int(argv[i])
     print("{:d}".format(count))
