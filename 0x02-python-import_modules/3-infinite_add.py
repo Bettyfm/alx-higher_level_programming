3c2cb749b1d956437df786c34ddca599a23dc475
Bettyfm <bettyfraigui@gmail.com>
2023-11-30 15:12:30 -0800

    test

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    length = len(argv)
+    count = 0
+    for i in range (1, length):
+        count += int(argv[i])
+    print("{:d}".format(count))
